7055070a9594630473d4b3b45eb52082cf979f9f
timothyheyer <theyer1@msudenver.edu>
2019-03-04 18:25:13 -0700

    added create-new-file with microbit edits

---
+++ Hello_World.py
@@ -0,0 +1,2 @@
+from microbit import *
+display.scroll("My name is Timothy Heyer!")
--- create-new-file.py
+++ create-new-file.py
@@ -1,3 +1,10 @@
 from microbit import *
 
-display.scroll("My name is Timothy Heyer!")
+while True:
+    reading = accelerometer.get_x()
+    if reading > 20:
+        display.show("R")
+    elif reading < -20:
+        display.show("L")
+    else:
+        display.show("-")
